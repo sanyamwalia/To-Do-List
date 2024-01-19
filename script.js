c98b1861dbf36300aeebf4600c526e07d00473cc
Sanyam <sanyam9896@gmail.com>
2024-01-20 00:23:12 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let todoList = [
+    {item: 'Buy Milk', dueDate: '04/02/2024'}, {item: 'Go to college', dueDate: '04/02/2024'}
+];
+displayItems();
+
+function addToDo()
+{
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate: todoDate});
+    inputElement.value = '';
+    dateElement.value = '',
+    displayItems();
+}
+
+function displayItems()
+{
+    let containerElement = document.querySelector('.todo-container');
+
+    let newHtml = '';
+    
+    for(let i = 0; i < todoList.length; i++)
+    {
+        let {item, dueDate} = todoList[i];
+        newHtml += `
+            <span>${item}</span>
+            <span>${dueDate}</span>
+            <button class="button-delete" onclick="todoList.splice(${i},1);
+            displayItems();">Delete</button>
+            `;
+    }
+    containerElement.innerHTML = newHtml;
+}
